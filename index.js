ce84cf1ed693312b12baecde5b657d4cf93d0a05
Alex Garcia <alejo_2986@hotmail.com>
2023-08-28 11:26:17 -0600

    Added: auth

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import jwt from "jsonwebtoken"
 import cors from 'cors'
 import productsRoutes from './app/routes/products.routes.js'
 import categoriesRoutes from './app/routes/categories.routes.js'
 import subcategoriesRoutes from './app/routes/subcategories.routes.js'
-import users from './app/routes//users.routes.js'
+import users from './app/routes/users.routes.js'
+import authentication from './app/routes/authentication.routes.js'
 //import path from 'node:path'
 //import * as url from 'url'
 
@@ -23,8 +25,15 @@ const PORT = process.env.PORT
 app.use('/api', productsRoutes)
 app.use('/api', categoriesRoutes)
 app.use('/api', subcategoriesRoutes)
-app.use('/api', users)
+app.use('/api', authentication)
+app.use('/', users)
 
+app.use((req, res) => {
+    res.status(404).json({
+        error: 0,
+        message: "Resource no found"
+    })
+})
 
 
 app.listen(PORT, () => {
