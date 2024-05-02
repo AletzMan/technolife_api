35ab5da252449aeefe1a2c2641d963c45cfc7a45
= <=>
2024-05-01 21:30:27 -0600

    chore: update files

--- app/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { ValidateUser, GetUsers } from '../controllers/users.controller.js'
-const router = Router()
-
-router.post('/users', ValidateUser)
-
-
-export default router
