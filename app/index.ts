b13d12034b72a95cb4d39a5b02674acf26c64ddc
= <=>
2024-05-01 21:00:58 -0600

    feat: add index files

---
+++ app/index.ts
@@ -0,0 +1,12 @@
+import { App } from "./app"
+import dotenv from "dotenv"
+
+dotenv.config()
+const PORT = process.env.PORT
+
+async function main() {
+	const app = new App(PORT)
+	app.listen()
+}
+
+main()
