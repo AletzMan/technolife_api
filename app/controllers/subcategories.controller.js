411a7159d1e892faa5c0f0bd52b5e825c054893b
= <=>
2024-05-01 20:38:42 -0600

    chore: delete files not used

--- app/controllers/subcategories.controller.js
+++
@@ -1,6 +0,0 @@
-import { pool } from "../../config/db.js"
-
-export const GetSubCategories = async (req, res) => {
-    const [response] = await pool.query('SELECT * FROM subcategories')
-    res.json(response)
-}
--- app/routes/subcategories.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { GetSubCategories } from '../controllers/subcategories.controller.js'
-
-const router = Router()
-
-router.get('/subcategories', GetSubCategories)
-
-export default router
