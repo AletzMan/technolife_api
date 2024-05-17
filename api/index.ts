243ea7e734880ef7c33cda164640e44c605df298
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-05-17 13:14:01 -0600

    Fixed: Route path

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "cors"
+//import cors from "cors"
 import morgan from "morgan"
 import dotenv from "dotenv"
 import users from "../routes/users.routes"
@@ -18,7 +18,7 @@ import coupons from "../routes/coupons.routes"
 import main from "../routes/main.routes"
 import swaggerUi from "swagger-ui-express"
 import swaggerSpec from "../config/swaggerConfig"
-const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath()
+//const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath()
 
 dotenv.config()
 const PORT = process.env.PORT
@@ -28,21 +28,26 @@ const app = express()
 dotenv.config()
 app.use(morgan("dev"))
 app.use(express.json())
-app.use(express.static(swaggerUiAssetPath))
+//app.use(express.static(swaggerUiAssetPath))
 app.use(express.urlencoded({ extended: true }))
-app.use(
+app.use(express.static(__dirname))
+/*app.use("*.css", (_req, res, next) => {
+	res.set("Content-Type", "text/css")
+	next()
+})*/
+/*app.use(
 	cors({
 		origin: process.env.FRONT_END_URL,
 		credentials: false,
 		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 	})
-)
-app.use(function (_req, res, next) {
+)*/
+/*app.use(function (_req, res, next) {
 	res.header("Content-Type", "application/json;charset=UTF-8")
 	res.header("Access-Control-Allow-Credentials", "true")
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	next()
-})
+})*/
 
 app.use("/", main)
 app.use("/api", users)
@@ -58,16 +63,11 @@ app.use("/api", orders)
 app.use("/api", brand)
 app.use("/api", customers)
 app.use("/api", coupons)
-app.use(
-	"/api-docs",
-	swaggerUi.serve,
-	swaggerUi.setup(swaggerSpec, {
-		customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
-	})
-)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, () => {
 	console.log("Server running at PORT: ", PORT)
+	//console.log(path.join(__dirname, "../routes/*"))
 }).on("error", (error) => {
 	// gracefully handle error
 	throw new Error(error.message)
